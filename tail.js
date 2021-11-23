9aba330bdb8f4b3364811b3e579bc569a3903e6c
Ray Flores <ray.m.flores@gmail.com>
2021-11-23 03:00:42 +0000

    checking errors in tail.js

--- tail.js
+++ tail.js
@@ -1,13 +1,13 @@
+const tail = function(array) {
+  return array.slice(1);
+};
+
 const assertEqual = function(actual, expected) {
   if (actual === expected) {
     console.log(`✅ Assertion Passed: ${actual} === ${expected}`);
   } else if (actual !== expected) {
     console.log(`🛑 Assertion Failed: ${actual} !== ${expected}`);
   }
-};
-
-const tail = function(array) {
-  return array.slice(1);
 };
 
 const words = ["Yo Yo", "Lighthouse", "Labs"];
