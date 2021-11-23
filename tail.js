40f09198db11c02cbfe2c8e8ad1b00667c4d8911
Ray Flores <ray.m.flores@gmail.com>
2021-11-23 02:19:42 +0000

    created a tail function and showed that our assertEqual function has limitations when comparing two arrays

---
+++ tail.js
@@ -0,0 +1,24 @@
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`✅ Assertion Passed: ${actual} === ${expected}`);
+  } else if (actual !== expected) {
+    console.log(`🛑 Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+
+const tail = function(array) {
+  return array.slice(1);
+};
+
+const words = ["Yo Yo", "Lighthouse", "Labs"];
+tail(words);
+assertEqual(words.length, 3);
+
+const one = [1];
+tail(one);
+assertEqual(tail(one), []);
+
+const empty = [];
+tail(empty);
+assertEqual(tail(empty), []);
+
