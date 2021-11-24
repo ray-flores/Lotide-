0b87fd0bc721c8d72a6494550facdc9c916c8d27
Ray Flores <ray.m.flores@gmail.com>
2021-11-24 14:44:55 +0000

    implement assertArrayEqual

---
+++ assertArrayEqual.js
@@ -0,0 +1,24 @@
+const eqArrays = function(arr1, arr2) {
+  if (arr1.length !== arr2.length) {
+    console.log(`🛑 Length Assertion Failed: ${arr1} !== ${arr2}`);
+    return false;
+  }
+  for (let i = 0; i <= arr1.length; i++) {
+    if(arr1[i] !== arr2[i]) {
+      console.log(`🛑 Element Assertion Failed: ${arr1} !== ${arr2}`);
+      return false;
+    }
+  }
+  console.log(`✅ Assertion Passed: ${arr1} === ${arr2}`);
+  return `true`;
+};
+
+/*
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`✅ Assertion Passed: ${actual} === ${expected}`);
+  } else if (actual !== expected) {
+    console.log(`🛑 Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+*/
