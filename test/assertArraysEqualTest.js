6870446c57b56fe2d46a52be136c2da9c5c26f87
Ray Flores <ray.m.flores@gmail.com>
2021-11-29 23:31:19 +0000

    refactored assertArraysEqual to export function and moved test code to assertArraysEqualTest.js using require to maintain functionality

---
+++ test/assertArraysEqualTest.js
@@ -0,0 +1,12 @@
+const eqArrays = require('../eqArrays');
+const assertArraysEqual = require('../assertArraysEqual');
+
+assertArraysEqual([1, 2, 3], [1, 2, 3]);
+
+// const assertEqual = function(actual, expected) {
+//   if (actual === expected) {
+//     console.log(`✅ Assertion Passed: ${actual} === ${expected}`);
+//   } else if (actual !== expected) {
+//     console.log(`🛑 Assertion Failed: ${actual} !== ${expected}`);
+//   }
+// };
