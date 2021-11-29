13a87993c66ea7beac52307a2f51475dd23a3886
Ray Flores <ray.m.flores@gmail.com>
2021-11-29 23:22:21 +0000

    refactored eqArrays.js and moved test code to eqArraysTest.js keeping its functionality by using module.exports and require

---
+++ test/eqArraysTest.js
@@ -0,0 +1,16 @@
+const eqArrays = require('../eqArrays');
+const assertEqual = require('../assertEqual');
+
+
+eqArrays([1, 2, 3], [1, 2, 3]); 
+eqArrays([1, 2, 3], [3, 2, 1]); 
+
+eqArrays(["1", "2", "3"], ["1", "2", "3"]); 
+eqArrays(["1", "2", "3"], ["1", "2", 3]); 
+
+assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true); 
+assertEqual(eqArrays([1, 2, 3, 4], [1, 2, 3]), false);
+assertEqual(eqArrays([1, 2, false], [1, 2, 3]), false);
+assertEqual(eqArrays([1, 2, 3], [1, undefined, 3]), false);
+assertEqual(eqArrays(["hi", 2, 3], [1, 2, 3]), false);   
+
