21a6ce4f1ce69e106fe297c2884d2fa9f6484d2b
Ray Flores <ray.m.flores@gmail.com>
2021-11-30 01:18:46 +0000

    refactored tailTest.js and middleTest.js to use mocha and chai

--- test/middleTest.js
+++ test/middleTest.js
@@ -1,17 +1,28 @@
 const middle = require('../middle');
-const assertArraysEqual = require('../assertArraysEqual');
+// const assertArraysEqual = require('../assertArraysEqual');
+const assert = require('chai').assert;
+
+describe("#middle", () => {
+  it("returns [2, 3] for [1, 2, 3, 4]", () => {
+    assert.deepEqual(middle([1, 2, 3, 4]), [2, 3]);
+  });
+
+  it("returns [2] for [1, 2, 3]", () => {
+    assert.deepEqual(middle([1, 2, 3]), [2]); 
+  });
+
+});
+
+// let arr = [1, 2, 6, 4, 5];
+// let arr2 = [1, "2", 3, 4, 5];
+// let arr3 = [1, 2, 3, 4];
+// let arr4 = [1, 2, 3, 4];
+
+// middle(arr);
+// middle(arr2);
+// middle(arr3);
 
 
-let arr = [1, 2, 6, 4, 5];
-let arr2 = [1, "2", 3, 4, 5];
-let arr3 = [1, 2, 3, 4];
-let arr4 = [1, 2, 3, 4];
-
-middle(arr);
-middle(arr2);
-middle(arr3);
-
-
-console.log(assertArraysEqual(middle(arr), [6]));
-console.log(assertArraysEqual(middle(arr4), [2, 3]));
-console.log(assertArraysEqual(middle(arr2),  [3]));
+// console.log(assertArraysEqual(middle(arr), [6]));
+// console.log(assertArraysEqual(middle(arr4), [2, 3]));
+// console.log(assertArraysEqual(middle(arr2),  [3]));
--- test/tailTest.js
+++ test/tailTest.js
@@ -1,16 +1,15 @@
-const assertEqual = require('../assertEqual');
+
+const assert = require('chai').assert;
 const tail = require('../tail');
 
+describe("#tail", () => {
+  it("returns [2, 3] for [1, 2, 3]", () => {
+    assert.deepEqual(tail([1, 2, 3]), [2, 3]);
+  });
 
-const words = ["Yo Yo", "Lighthouse", "Labs"];
-tail(words);
-assertEqual(words.length, 3);
+  it("returns [] for [1]", () => {
+    assert.deepEqual(tail([1]), []); 
+  });
 
-const one = [1];
-tail(one);
-assertEqual(tail(one), []);
-
-const empty = [];
-tail(empty);
-assertEqual(tail(empty), []);
+});
 
