13aa2d1399317aba5dfc55cceaa8667d5283f296
Ray Flores <ray.m.flores@gmail.com>
2021-11-30 00:49:45 +0000

    refactored headTest.js to use chai and mocha, as well as created a .gitignore to remove node_modules from being committed

--- test/headTest.js
+++ test/headTest.js
@@ -1,6 +1,13 @@
-const assertEqual = require('../assertEqual');
+const assert = require('chai').assert;
 const head = require('../head');
 
+describe("#head", () => {
+  it("returns 1 for [1, 2, 3]", () => {
+    assert.strictEqual(head([1, 2, 3]), 1);
+  });
 
-assertEqual(head([7]), 6);
-assertEqual(head([]), undefined);
+  it("returns '5' for ['5']", () => {
+    assert.strictEqual(head(['5']), '5'); 
+  });
+
+});
